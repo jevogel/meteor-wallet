2bdcabb0650498e316de3dd66b6bfa639e901ea6
Jimmy Vogel <jim.e.vogel@gmail.com>
2022-03-16 10:12:28 -0400

    Add client api imports for optimistic ui

--- server/main.js
+++ server/main.js
@@ -2,6 +2,4 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/ContactsCollection';
 import '../imports/api/ContactsMethods';
 
-Meteor.startup(() => {
-
-});
+Meteor.startup(() => {});
